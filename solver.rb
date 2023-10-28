aefcc278648c44bba56b758bc3fcd26bbad8c69d
faisalakb <faisalakb786@gmail.com>
2023-10-28 14:57:13 +0500

    Update linters

--- solver.rb
+++ solver.rb
@@ -1,32 +1,29 @@
 class Solver
-    def factorial(n)
-      raise ArgumentError, "Input must be a non-negative integer" if n < 0
-      return 1 if n == 0
-      (1..n).reduce(:*)
-    end
-  
-    def reverse(word)
-      word.reverse
-    end
-  
-    def fizzbuzz(n)
-      case
-      when n % 3 == 0 && n % 5 == 0
-        "fizzbuzz"
-      when n % 3 == 0
-        "fizz"
-      when n % 5 == 0
-        "buzz"
-      else
-        n.to_s
-      end
+  def factorial(n)
+    raise ArgumentError, 'Input must be a non-negative integer' if n < 0
+    return 1 if n.zero?
+    (1..n).reduce(:*)
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(n)
+    if (n % 3).zero? && (n % 5).zero?
+      'fizzbuzz'
+    elsif (n % 3).zero?
+      'fizz'
+    elsif (n % 5).zero?
+      'buzz'
+    else
+      n.to_s
     end
   end
-  
-  
-  solver = Solver.new
-  puts solver.factorial(5)     
-  puts solver.reverse("hello")
-  puts solver.fizzbuzz(15)     
-  puts solver.fizzbuzz(7)      
-  
+end
+
+solver = Solver.new
+puts solver.factorial(5)
+puts solver.reverse('hello')
+puts solver.fizzbuzz(15)
+puts solver.fizzbuzz(7)
